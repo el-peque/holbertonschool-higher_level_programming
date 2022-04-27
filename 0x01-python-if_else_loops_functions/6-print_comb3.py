d04b65696ae514b457f57a38c2a42d3354c66d10
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-04-26 17:43:21 -0700

    Update 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for n in range(0, 10):
+        if i >= n:
+            continue
+        if i < 8:
+            print(f"{i}{n}", end = ", ")
+        else:
+            print(f"{i}{n}")
