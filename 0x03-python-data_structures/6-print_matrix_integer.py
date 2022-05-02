4e698de744fc87f3f88facd8384e3ad6032e78b2
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-02 10:28:54 -0700

    Added 6-print_matrix_integers.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for val in row:
-            print(val, end="")
-            if (val + 1 in row):
-                print(" ", end="")
+    if matrix:
+        for row in matrix:
+            if val in row:
+                for val in row:
+                    print(f"{val:d}", end="")
+                    if (val + 1 in row):
+                        print(" ", end="")
         print("$")
