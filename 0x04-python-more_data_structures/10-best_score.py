a814ee4e0b3346e2ddb0690559c74c549d56e791
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-04 15:35:37 -0700

    Added 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        max = list(a_dictionary.values())[0]
+        for k, v in a_dictionary.items():
+            if v > max:
+                max = v
+                s = k
+        return s
+    else:
+        return None
