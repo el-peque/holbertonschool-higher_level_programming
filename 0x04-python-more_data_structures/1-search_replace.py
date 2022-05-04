839dc865f39d30e03d582c9cc775dd5078d8909e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-04 07:05:40 -0700

    Added 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(new_list.count(search)):
+        my_list[new_list.index(search)] = replace
+    return new_list
