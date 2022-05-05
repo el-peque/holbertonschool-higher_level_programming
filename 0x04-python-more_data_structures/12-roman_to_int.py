3a483e7b3836c1daaa844131899b907180adcd91
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-05-05 01:53:38 -0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
-        return None
+        return 0
     res = 0
     i = -1
     s = roman_string
