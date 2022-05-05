8a489d0b25263e632831d5a0234fefe66c552f66
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-04 17:34:23 -0700

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,12 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return None
     res = 0
-    i = 0
+    i = -1
     s = roman_string
     roman_num = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     for c in roman_string:
+        if roman_num[(roman_string[i])] < roman_num[c] and i >= 0:
+            res -= roman_num[(roman_string[i])]
         res += roman_num[c]
-    i = s.count("IV") + s.count("IX") + s.count("IL") + s.count("IC")
-    i += s.count("ID") + s.count("IM")
-    res += i * (-2)
+        i += 1
     return int(res)
