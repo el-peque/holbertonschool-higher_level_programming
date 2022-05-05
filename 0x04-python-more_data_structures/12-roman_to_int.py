b82ac21b52ab429250c00d382d63000900e58911
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-04 17:40:55 -0700

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     roman_num = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     for c in roman_string:
         if roman_num[(roman_string[i])] < roman_num[c] and i >= 0:
-            res -= roman_num[(roman_string[i])]
+            res -= roman_num[(roman_string[i])] * 2
         res += roman_num[c]
         i += 1
     return int(res)
