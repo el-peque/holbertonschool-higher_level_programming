d16d44f4b12ff8cbb1b24263592d363d3973f8aa
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-17 16:46:28 -0700

    Added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""declaration"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size=0):
+        """size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+
+        self.__size = size
