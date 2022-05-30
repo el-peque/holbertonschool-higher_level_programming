506418b8ef4b9c4bbaec1f3670167b2f4e9f8666
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-30 14:46:38 -0700

    Create 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry():
+    """BaseGeometry"""
+
+    def area(self):
+        """Area not implemented"""
+        raise Exeption("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
