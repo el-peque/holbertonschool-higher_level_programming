efee63eb00af27d188fa76a377df564e770f949e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-30 14:46:17 -0700

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry():
+    """BaseGeometry"""
+
+    def area(self):
+        """Area not implemented"""
+        raise Exeption("area() is not implemented")
