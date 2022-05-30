79daa442182eedf19c41c1cf4e26d1f87688b106
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-30 14:41:17 -0700

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""function declaration"""
+
+
 def is_same_class(obj, a_class):
+    """checks if its the same class"""
     if type(obj) == a_class:
         return True
     else:
