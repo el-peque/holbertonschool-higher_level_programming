318d6cf948ca1eda6b64d524d3abca08f9d0231c
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-31 14:33:51 -0700

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,9 +3,7 @@
 
 
 def read_file(filename=""):
-    """writes a string to a text file (UTF8) and returns
-    the number of characters written"""
-
+    """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
