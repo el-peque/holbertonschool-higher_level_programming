40eff0995edc9c6a6053192506156c44d7bf98eb
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-30 18:12:09 -0700

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,10 +6,13 @@ class MyInt(int):
     """class MyInt that inherits from int"""
 
     def __init__(self, num):
+        """initializes"""
         self.__num = num
 
     def __eq__(self, n):
+        """changes behavior of =="""
         return super().__ne__(n)
 
     def __ne__(self, n):
+        """changees behavior off !="""
         return super().__eq__(n)
