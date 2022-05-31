7ab8c087caa464540a5199c5a5092219a0985d1d
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-31 14:32:25 -0700

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Function declaration"""
+
+
 def read_file(filename=""):
+    """writes a string to a text file (UTF8) and returns
+    the number of characters written"""
+
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
