477d8f8389ecf431c372d4e7571f29cd61b9d57e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-05-31 18:33:37 -0700

    Create 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class declaration"""
+
+
+class Student():
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if type(attrs) == list and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    d[key] = self.__dict__.get(key)
+            return d
+        return self.__dict__
