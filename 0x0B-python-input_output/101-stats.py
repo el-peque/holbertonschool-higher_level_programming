4236ad5cd7805b499b8284d8e0f755f14958ff58
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-06-01 09:44:05 -0700

    Create 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,9 +7,23 @@ import shlex
 
 
 i = 0
+b = []
+total_size = 0
 for line in fileinput.input():
-    a = shlex.split(line)
-    print(f"{a[5]}: {a[3]}")
     if i == 10:
+        b.sort()
+        for n in b:
+            total_size += n
+            print(f"b: {b}")
         print(f"File size: {total_size}")
+        total_size = 0
+        b = []
+        i = 0
+        continue
+    a = shlex.split(line)
+    b += [a[5]]
+#    total_size += inta[6])
     i += 1
+#    for i in b:
+#        print(f"{i}: {a[6]}")
+    
