baf54c141a21e8fc65a954888418122d9881304e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-06-05 09:30:01 -0700

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,29 @@ class Rectangle(Base):
         """ returns [Rectangle] (<id>) <x>/<y> - <width>/<height> """
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
 {self.__width}/{self.__height}"
+
+    def update(self, *args, **kwargs):
+        """ assigns an argument to each attribute """
+        if args and len(args) > 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs.get('id')
+            if 'width' in kwargs:
+                self.width = kwargs.get('width')
+            if 'height' in kwargs:
+                self.height = kwargs.get('height')
+            if 'x' in kwargs:
+                self.x = kwargs.get('x')
+            if 'y' in kwargs:
+                self.y = kwargs.get('y')
+                
