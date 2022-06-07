5e56b0c7e250dc8fa40442e38e473cbd4d0827b7
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-06-07 06:51:48 -0700

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base """
 import json
+import csv
 
 
 class Base():
@@ -52,6 +53,34 @@ class Base():
     @classmethod
     def load_from_file(cls):
         """ returns a list of instances """
+        try:
+            with open(cls.__name__ + ".json", mode="r", encoding="utf-8") as f:
+                data = f.read()
+            list = cls.from_json_string(data)
+            return [cls.create(**i) for i in list]
+
+        except IOError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ serializes in CSV """
+        if list_objs is None:
+            list = []
+        else:
+            list = [i.to_dictionary() for i in list_objs]
+        with open(cls.__name__ + ".csv", mode="w", encoding="utf-8") as f:
+            for dic in list:
+                writer = csv.writer(f)
+                if cls.__name__ == "Rectangle":
+                    f.write(f"{dic.get('id')},{dic.get('width')},\
+{dic.get('height')},{dic.get('x')},{dic.get('y')}")
+                if cls.__name__ == "Square":
+                    f.write(f"{dic.get('id')},{dic.get('size')},{dic.get('x')},{dic.get('y')}")
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ deserializes in CSV """
         try:
             with open(cls.__name__ + ".json", mode="r", encoding="utf-8") as f:
                 data = f.read()
