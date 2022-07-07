cd1fc7362c20d8a8737028241112d95f5c0f3264
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-07-06 21:37:37 -0300

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT name AS genre, COUNT(genre_id) AS number_of_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY genre ORDER BY number_of_shows DESC;
+SELECT name AS genre, COUNT(genre_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON id = genre_id GROUP BY genre ORDER BY number_of_shows DESC;
