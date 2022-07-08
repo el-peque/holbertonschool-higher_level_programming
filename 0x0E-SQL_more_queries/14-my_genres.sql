929d960cf4dbf71cfd58a6decc7d131d9c9804f0
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-07 18:17:13 -0700

    Create 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT name AS genre, COUNT(genre_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON id = genre_id GROUP BY genre ORDER BY number_of_shows DESC;
+SELECT name FROM tv_genres JOIN tv_show_genres ON tv_genres.id = genre_id JOIN tv_shows ON tv_shows.id = show_id WHERE title = 'Dexter' ORDER BY name ASC;
