c6b65708aa7c8a9a7bb5ede33ada6adcd7556944
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-01 15:46:13 -0700

    Fixed 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const myArgs = process.argv.slice(2);
 const num = parseInt(myArgs[0]);
 if (num) {
-  console.log(num);
+  console.log('My number: ' + num);
 } else {
   console.log('Not a number');
 }
