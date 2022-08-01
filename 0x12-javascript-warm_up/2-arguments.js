dc8d4a752e29c68b93a63693c530529cc0cd28d4
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-01 15:34:54 -0700

    Fixed 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-if (myArgs[0]) {
+if (myArgs[1]) {
+  console.log('Arguments found');
+} else if (myArgs[0]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
