e640b3932698d76d1043923e5c5f6b25d0421d25
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-01 18:16:14 -0700

    Create 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+let maxNum = parseInt(myArgs[0]);
+let auxNum = parseInt(myArgs[0]);
+let num; let i;
+
+if (myArgs.length < 2) {
+  console.log(0);
+} else {
+  for (i = 0; i < myArgs.length; i++) {
+    num = parseInt(myArgs[i]);
+    if (num > maxNum) {
+      maxNum = num;
+    }
+    if (num > auxNum && num <= maxNum && num < parseInt(myArgs[i + 1])) {
+      auxNum = num;
+    }
+  }
+  console.log(auxNum);
+}
