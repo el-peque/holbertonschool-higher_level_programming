ecc55e7fcdbfe0bfe115bdb9376e6532eda7854a
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-07 16:07:50 -0700

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,6 +4,8 @@ state, using the database hbtn_0e_4_usa """
 import MySQLdb
 import sys
 from model_state import Base, State
+
+
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
