e60f926cc4dc45089300b423bd44703eb103204c
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-19 11:33:47 -0700

    Fixed task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
 from urllib.request import urlopen
+
+
 with urlopen('https://intranet.hbtn.io/status') as response:
     data = response.read()
     print(f"Body response:\n\
