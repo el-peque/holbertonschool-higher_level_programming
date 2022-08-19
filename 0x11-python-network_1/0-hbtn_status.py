2bb8de382b29d64e45db679186d0fd86678a1c4b
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-08-19 11:32:18 -0700

    Fixed task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,6 @@ from urllib.request import urlopen
 with urlopen('https://intranet.hbtn.io/status') as response:
     data = response.read()
     print(f"Body response:\n\
-    - type: {type(data)}\n\
-    - content: {data}\n\
-    - utf8 content: {data.decode('utf8')}")
+\t- type: {type(data)}\n\
+\t- content: {data}\n\
+\t- utf8 content: {data.decode('utf8')}")
