9b369a3fde5ce0a5b1371c43e24cdd170d5a3c71
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-12 16:45:37 -0700

    Fix task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,17 +2,12 @@
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const axios = require('axios').default;
 
-function getCharacter (url) {
-  axios.get(url)
-    .then(function (response) {
-      console.log(response.data.name);
-    });
-}
-
 axios.get(url)
   .then(function (response) {
     let character;
     for (character of response.data.characters) {
-      getCharacter(character);
-    }
+    axios.get(character)
+      .then(function (response) {
+        console.log(response.data.name);
+    })}
   });
