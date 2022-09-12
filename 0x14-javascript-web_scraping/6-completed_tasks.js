a3a88bfb355f2989511410019b236a3e3c4f6c17
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-12 15:06:42 -0700

    Create 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const axios = require('axios').default;
+axios.get(url)
+  .then(function (response) {
+    const userTask = {};
+    let taskCount = 0;
+    let task;
+    let userId = response.data[0].userId;
+    for (task of response.data) {
+      if (task.userId !== userId) {
+        userTask[userId] = taskCount;
+        userId = task.userId;
+        taskCount = 0;
+      }
+      if (task.completed === true) {
+        taskCount += 1;
+      }
+    }
+    userTask[userId] = taskCount;
+    console.log(userTask);
+  });
