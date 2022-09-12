ef4a331accf863a5bb11e52b57e0a3382ee661fa
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-12 12:31:33 -0700

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+
 const url = process.argv[2];
 const axios = require('axios').default;
-let movie; let count = 0;
 axios.get(url)
   .then(function (response) {
+    let movie;
+    let count = 0;
     for (movie of response.data.results) {
       if (movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
         count += 1;
